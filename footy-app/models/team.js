830b125b8ac5e716c1c0b9e9af072253fa81de58
Kurt <kujo8p@gmail.com>
2023-05-28 12:24:26 -0500

    stat Schema

--- footy-app/models/team.js
+++ footy-app/models/team.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const statsSchema = new Schema({
+  matches: Number,
+  goals: Number,
+  assists: Number
+})
+
 const playerSchema = new Schema({
   number: {
     type: Number,
@@ -13,5 +19,8 @@ const playerSchema = new Schema({
   position: {
     type: String,
     required: true
-  }
+  },
+  stats: [statsSchema]
 })
+
+module.exports = mongoose.model('Player', playerSchema)
